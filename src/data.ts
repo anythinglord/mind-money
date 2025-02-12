4f8bb1d446f1e824eb48a8003ffe1e1640f85cf0
anythinglord <sjsalcedoa@unal.edu.co>
2025-02-12 11:01:21 -0500

    create sections, interfaces, modify fonts

---
+++ src/data.ts
@@ -0,0 +1,8 @@
+import { Section } from './interfaces';
+
+export const HomePageSections: Section[] = [
+    { name: 'Budget', icon: 'wallet', value: 5000 },
+    { name: 'Income', icon: 'dollar', value: 6000 },
+    { name: 'Expenses', icon: 'wallet', value: 5000 },
+    { name: 'Saving', icon: 'wallet', value: 5000 }
+]
