16aa8b9201d9814c7834ae866c2784bff907c502
anythinglord <sjsalcedoa@unal.edu.co>
2025-02-16 21:17:01 -0500

    modify login card

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -9,7 +9,7 @@ const LoginPage = () => {
                 <div className="title">login</div>
                 <div className="message">Enter your email and password to access your account</div>
                 <Input label='Email'/>
-                <Input type='password' label='password' />
+                <Input type='password' label='Password' />
                 <Button label='Log in' handleClick={() => alert('')}/>
                 <div className="message">Don't have an account? Sign up</div>
             </div>
