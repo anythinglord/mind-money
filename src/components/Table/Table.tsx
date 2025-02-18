6a79e0ece930659d6b1e70b50fa5ed224773162b
anythinglord <sjsalcedoa@unal.edu.co>
2025-02-18 10:40:15 -0500

    modify table style

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -11,12 +11,12 @@ export const Table = ({ data }: Props) => {
     console.log(data)
     return (
         <div className="table-index">
-            <div className="table-header table-row">
+            <div className="table-header flex-sp-cen">
                 {columns.map((column, index) => (<div className={`tbl-column ${index === 0 ? 'col-left' : ''}`}>{column}</div>))}
             </div>
             <div className="table-content">
                 {data.map(({ date, description, category, amount }) => (
-                    <div className="table-row">
+                    <div className="table-row flex-sp-cen">
                         <div className="tbl-column col-left">{date}</div>
                         <div className="tbl-column">{description}</div>
                         <div className="tbl-column">{category}</div>
