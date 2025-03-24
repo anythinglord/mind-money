e58e301371503d47ee2483683536eed8720108e4
anythinglord <sjsalcedoa@unal.edu.co>
2025-03-24 15:39:17 -0500

    remove useless code

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "../../components/Button";
 import { Input } from "../../components/Input";
 import { login, signup } from "../../services";
@@ -8,52 +8,33 @@ import { useNavigate } from "react-router-dom";
 import { useMutation } from '@tanstack/react-query';
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import * as z from "zod";
 import { isTrue } from "../../utils";
 import { userSchema } from "../../schemas";
 import { User } from "../../models";
 import "./index.css";
 
-const schema = z.object({
-    email: z.string().email("Invalid email"),
-    password: z.string()
-        .min(5, "Must be at least 12 characters long")
-        /*.regex(/^(?=.*[a-z])(?=.*[A-Z])/, "Must include upper case letters")
-        .regex(/[!@#$%^&*(),.?":{}|<>]/, "Must contain at least one special character")
-        .regex(/\d/, "Must contain at least one number"),*/,
-    confirm: z.string(),
-}).refine((data) => data.password === data.confirm, {
-    message: "Passwords must match",
-    path: ["confirm"],
-});
-
 const LoginPage = () => {
-    const [mode, setMode] = useState<string>('login');
+
+    const [isLogin, setIsLogin] = useState<boolean>(true);
     const [errorLogin, setErrorLogin] = useState<boolean>(false)
     const [errorSignUp, setErrorSignUp] = useState<boolean>(false)
 
     const { register, handleSubmit, formState: { errors } } = useForm({
-        resolver: zodResolver(userSchema(mode !== 'login')),
+        resolver: zodResolver(userSchema(!isLogin)),
     });
 
     const onSubmit = (data: any) => {
         try {
             const { email, password } = data;
-            if (mode === 'login') {
-                loginUser.mutate({ email: email, password: password })
-            }
-        } catch (error) {
+            isLogin ? loginUser.mutate({ email: email, password: password })
+                : signUpUser.mutate({ email: email, password: password })
 
+        } catch (error) {
+            throw new Error("Error on submit")
         }
-        console.log(data, mode);
     };
 
-    const onErrors = (errors: any) => console.error(errors);
-
-    const label = mode === 'login' ? 'Log in' : 'Sign up'
-    const emailRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const confirmPasswordRef = useRef<HTMLInputElement>(null);
+    const label = isLogin ? 'Log in' : 'Sign up'
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
@@ -66,17 +47,8 @@ const LoginPage = () => {
         onError: () => setErrorLogin(true),
     })
 
-    const mutation = useMutation({
-        mutationFn: () => login(emailRef.current?.value || '', passwordRef.current?.value || ''),
-        onSuccess: (data) => {
-            dispatch(setUser(data.user))
-            navigate('/home')
-        },
-        onError: () => setErrorLogin(true),
-    });
-
-    const mutationSignup = useMutation({
-        mutationFn: () => signup(emailRef.current?.value || '', passwordRef.current?.value || ''),
+    const signUpUser = useMutation({
+        mutationFn: (userData: User) => signup(userData.email, userData.password),
         onSuccess: (data) => {
             if (data.user) {
                 dispatch(setUser(data.user))
@@ -84,58 +56,34 @@ const LoginPage = () => {
             }
         },
         onError: (error) => alert("Error: " + error.message),
-    });
-
-    const handleMutation = () => {
-        if (mode === 'login') {
-            mutation.mutate()
-        } else {
-            const password = passwordRef.current?.value || ''
-            const confirmPassword = confirmPasswordRef.current?.value || ''
-            if (password === confirmPassword) {
-                setErrorSignUp(false)
-                mutationSignup.mutate()
-            } else {
-                setErrorSignUp(true)
-            }
-        }
-    }
-
-    /**
-     * Change mode between login and signup
-     * @returns void
-     */
-    const changeMode = () => {
-        setMode((prevMode) => {
-            return prevMode === 'login' ? 'signup' : 'login'
-        })
-    }
+    })
 
     useEffect(() => {
         setErrorLogin(false)
         setErrorSignUp(false)
-    }, [mode])
+    }, [isLogin])
 
     return (
-        <form onSubmit={handleSubmit(onSubmit, onErrors)}>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="root">
                 <div className="card">
                     <div className="title">{label}</div>
-                    <div className="message">Enter your email and password to {`${mode === 'login' ? 'access' : 'create'}`} your account</div>
+                    <div className="message">Enter your email and password to {`${isLogin ? 'access' : 'create'}`} your account</div>
                     <div className="error-message">{errorLogin && 'Invalid credentials'}</div>
+                    <div className="error-message">{errorSignUp && 'Server error'}</div>
                     <Input
                         label='email' register={register}
                         error={isTrue(errors.email)} errorMessage={errors.email?.message} />
                     <Input
                         type='password' label='password' register={register}
                         error={isTrue(errors.password)} errorMessage={errors.password?.message} />
-                    {mode !== 'login' &&
+                    {!isLogin &&
                         <Input
                             type='password' label='confirm' register={register}
                             error={isTrue(errors.confirm)} errorMessage={errors.confirm?.message} />}
-                    <Button label={`${label}`} /*handleClick={handleMutation}*/ />
+                    <Button label={`${label}`} />
                     <div className="message">
-                        Don't have an account? <a onClick={changeMode}>{mode === 'login' ? 'Sign up' : 'Log in'}</a>
+                        Don't have an account? <a onClick={() => setIsLogin((prevMode) => !prevMode)}>{isLogin ? 'Sign up' : 'Log in'}</a>
                     </div>
                 </div>
             </div>
