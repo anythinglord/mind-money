7f74fd03278dd0b5655231cd7c6ab3ec53edf1d2
anythinglord <sjsalcedoa@unal.edu.co>
2025-03-26 10:24:13 -0500

    add landing to routes, create a barrel for pages

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,7 @@
+import './index.css'
+
+export const Landing = () => {
+    return(
+        <div>landing</div>
+    )
+}
--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -3,6 +3,7 @@ import HomePage from '../pages/Home/Home';
 import LoginPage from '../pages/Login/Login';
 import PrivateRoute from './PrivateRoute';
 import RedirectRoute from './RedirectRoute';
+import { Landing } from '../pages';
 import { ExpensesPage } from '../pages/Expenses/Expenses';
 
 export const AppRoutes = () => {
@@ -10,7 +11,7 @@ export const AppRoutes = () => {
     <Routes>
       <Route path="/" element={<RedirectRoute />} >
         <Route path="login" element={<LoginPage />} />
-        <Route path="/" element={<LoginPage/>} />
+        <Route path="/" element={<Landing />} />
       </Route>
       <Route path="/" element={<PrivateRoute />} >
         <Route path="home" element={<HomePage />} />
