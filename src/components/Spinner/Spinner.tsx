defa6132c56108b93fa435932651f02c0a981e90
anythinglord <sjsalcedoa@unal.edu.co>
2025-04-14 10:20:36 -0500

    add backdrop while login or logout finnish

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -11,8 +11,8 @@ export const Spinner = ({ size }: Props) => {
     const getDimensions = (size: Size) => {
         
         const dimension = {     
-            'small' : { width: 24, height: 24 },
-            'medium': { width: 48, height: 48 },
+            'small' : { width: 16, height: 16 },
+            'medium': { width: 64, height: 64 },
             'large': { width: 128, height: 128 },
         }
         return dimension[size]
@@ -20,7 +20,7 @@ export const Spinner = ({ size }: Props) => {
 
     return (
         <svg className="spinner" viewBox="22 22 44 44" style={getDimensions(size)}>
-            <circle className="path" cx="44" cy="44" r="20.2" fill="none" stroke-width="3.6" />
+            <circle className="path" cx="44" cy="44" r="20.2" fill="none" strokeWidth="3.6" />
         </svg>
     )
 }
